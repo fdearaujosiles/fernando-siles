ae86e771062a650caeac7239ad4831572ee212d3
Carlos Henrique Coelho Filho <ins_ccoelho@uolinc.com>
2021-02-07 02:00:58 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">A Portfolio-ish</h1>
+
+<div align="center">
+   Solutions for challenges from Code Learning Sites.
+</div>
+
+## What's This?
+
+This is basically my HTML+CSS portfolio.
+I wanted to learn better CSS and make some nice pages, so I started this little personal project.
+
+[You can access it through here](https://fernando-siles.herokuapp.com/)
+
+[@fdearaujosiles](https://twitter.com/fdearaujosiles)
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/other', (req, res) => res.render('pages/other'))
+    .get('*', (req, res) => res.render('pages/404'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
