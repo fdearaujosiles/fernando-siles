c899ead4d18f8f605ca8503f0222a7f8a2884b80
Fernando Siles <siles.fernandoaraujo@gmail.com>
2021-02-22 23:28:30 -0300

    "Adjusting route"

--- router.js
+++ router.js
@@ -5,13 +5,13 @@ router.route('/')
 
 router.route('/pages')
     .get( (req, res) => res.render('pages/pages'))
-    .post((req, res) => res.json(
+    .post((req, res) => res.json(JSON.parse(`
         [
             {title: "Home", url: "/"},
             {title: "Pages", url: "/pages"},
             {title: "404", url: "*"},
         ]
-    ))
+    `)))
 
 router.get('*', (req, res) => res.render('pages/404'));
 
